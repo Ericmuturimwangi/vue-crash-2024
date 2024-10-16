67bed6ac4215244c83b3e425dc00fe41a350de63
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-10-16 19:52:12 +0300

    job_portal project_ added the navbar, the developer and employer cards

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import { resolve } from 'path';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
+  plugins: [vue()],
+  resolve:{
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@' : resolve(__dirname, 'src')
     }
-  }
-})
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer, 
+      ],
+    },
+  },
+});
