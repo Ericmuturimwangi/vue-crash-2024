f60df0001e64dcab3ecef2ef83b80242973e3f6d
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-10-16 22:16:12 +0300

    adding the routes and the views

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
-import Hero from '@/components/Hero.vue';
-import HomeCards from './components/HomeCards.vue';
-import JobListings from './components/JobListings.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <Navbar />
-  <Hero  />
-  <HomeCards /> 
-  <!-- show 3 jobs a page -->
-  <JobListings :limit="3" :showButton="true"/>  
+  <RouterView />
   </template>
